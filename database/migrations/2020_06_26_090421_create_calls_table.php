06be38cdf3bd407884b512d12161d603281802a0
Abu Sufian <sufian_ict07@yahoo.com>
2020-06-27 12:48:21 +0600

    Database and model

---
+++ database/migrations/2020_06_26_090421_create_calls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calls', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('call_date');
+            $table->string('phone_number');
+            $table->time('call_duration');
+            $table->enum('status', ['in_call', 'hold', 'call_back', 'do_not_call']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calls');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $faker = Faker::create();
+        for ($i=0; $i < 500; $i++) {
+            \App\Call::create([
+                'call_date' => date('Y-m-d', strtotime( '+'.mt_rand(0,30).' days')),
+                'phone_number' => $faker->phoneNumber,
+                'call_duration' =>  rand(0,23).":".str_pad(rand(0,59), 2, "0", STR_PAD_LEFT),
+                'status' => Arr::random(['in_call', 'hold', 'call_back', 'do_not_call']),
+            ]);
+        }
     }
 }
